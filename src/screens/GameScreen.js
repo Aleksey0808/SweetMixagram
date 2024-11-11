4485ba79dd7d62afc9451cd92d8619a4bcd5236f
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2024-11-11 10:05:32 +0200

    add timer, pause, mix,coins,hunt,skip

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -3,74 +3,124 @@ import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert } from 'react
 import wordsData from '../helpers/wordsData';
 
 const GameScreen = () => {
+  const [timer, setTimer] = useState(60);
   const [coins, setCoins] = useState(0);
-  const [life, setLife] = useState(3);
   const [guessedWords, setGuessedWords] = useState([]);
   const [selectedLetters, setSelectedLetters] = useState('');
   const [shuffledLetters, setShuffledLetters] = useState([]);
-  // console.log('selectedLetters', selectedLetters)
-  console.log('guessedWords', guessedWords)
-
-  // Берем первый элемент уровня `easy`
-  const currentWordData = wordsData.easy[0];
-  // const res = currentWordData.words.map(word => word.toLowerCase())
-  // console.log(res.includes(selectedLetters))
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+ 
+  const currentWordData = wordsData.easy[currentWordIndex]
 
   useEffect(() => {
-    // Перемешиваем буквы текущего слова
+    let interval;
+    if (!paused && timer > 0) {
+      interval = setInterval(() => setTimer((prev) => prev - 1), 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [paused, timer]);
+
+  useEffect(() => {
+    if (currentWordIndex === 2 && currentWordData.words.length === guessedWords.length) {
+      return  Alert.alert('YOU WIN!');
+    }
     setShuffledLetters(shuffleWord(currentWordData.word));
-  }, []);
+    if (currentWordData.words.length === guessedWords.length) {
+      setCurrentWordIndex(currentWordIndex + 1)
+      setGuessedWords([])
+    }
+  }, [guessedWords]);
 
   const shuffleWord = (word) => {
     return word.split('').sort(() => Math.random() - 0.5);
   };
 
+  const clean = () => {
+    setSelectedLetters('')
+  }
+
   const handleLetterPress = (letter) => {
     const newSelectedLetters = selectedLetters + letter;
-    console.log('newSelectedLetters', newSelectedLetters)
     setSelectedLetters(newSelectedLetters);
-    
-    // console.log('new', newSelectedLetters.length)
-    // console.log('current', currentWordData.words[0].length)
 
-    // Проверяем слово, если длина совпадает с допустимой
     if (newSelectedLetters.length === currentWordData.words[0].length) {
       handleGuess(newSelectedLetters);
     }
   };
 
   const handleGuess = (newSelectedLetters) => {
-    const normalizedInput = selectedLetters.toLowerCase();
     const normalizedWords = currentWordData.words.map(word => word.toLowerCase());
     console.log('guessedWords', guessedWords.includes(newSelectedLetters))
 
     if (normalizedWords.includes(newSelectedLetters) && !guessedWords.includes(newSelectedLetters)) {
-    // if (normalizedWords.includes(newSelectedLetters)) {
       setGuessedWords([...guessedWords, newSelectedLetters]);
       setCoins(coins + 10);
       setSelectedLetters('');
     } else if (guessedWords.includes(newSelectedLetters)) {
       Alert.alert('Такое слово уже есть.');
-      setLife(life - 1)
       setSelectedLetters(''); 
     } else {
       Alert.alert('Неверное слово', 'Попробуйте еще раз.');
-      setLife(life - 1)
       setSelectedLetters(''); 
     }
+  };
+
+  const handleSkip = () => {
+    const wordToSkip = currentWordData.words.find((word) => !guessedWords.includes(word));
+    if (wordToSkip) {
+      setGuessedWords([...guessedWords, "skip"]);  
+    }
+  };
+  
+  const handleHint = () => {
+    console.log('Hint')
+    const remainingWords = currentWordData.words.filter(word => !guessedWords.includes(word));
+
+    if (remainingWords.length > 0) {
+      const hintWord = remainingWords[0];
+      setGuessedWords([...guessedWords, hintWord]);
+    } else {
+      Alert.alert("Все слова угаданы!");
+    }
+  };
+
+  const selectPaused = () => {
+    setPaused(!paused);
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
+      <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 20}}>
+      <Text style={styles.coins}>Time: {timer}</Text>
         <Text style={styles.coins}>Coins: {coins}</Text>
-        <Text style={styles.coins}>Life: {life}</Text>
+        <TouchableOpacity onPress={selectPaused}>
+        <Text style={styles.coins}>{paused ? 'Resume' : 'Pause'}</Text>
+        </TouchableOpacity>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 20}}>
+          <TouchableOpacity onPress={handleSkip}>
+          <Text style={styles.skipButton}>SKIP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleHint}>
+          <Text style={styles.hintButton}>hunt</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShuffledLetters(shuffleWord(currentWordData.word))}>
+              <Text style={styles.letter}>mix</Text>
+        </TouchableOpacity>
+        </View>
+        
 
         <FlatList
           data={shuffledLetters}
           renderItem={({ item }) => (
             <TouchableOpacity onPress={() => handleLetterPress(item)}>
-              <Text style={styles.letter}>{item}</Text>
+              <Text style={styles.letter}>
+              {item === currentWordData.words ? 'skip' : item}
+              </Text>
             </TouchableOpacity>
           )}
           keyExtractor={(item, index) => index.toString()}
@@ -81,13 +131,17 @@ const GameScreen = () => {
         <View style={styles.inputContainer}>
           <Text style={styles.inputText}>{selectedLetters}</Text>
         </View>
+            <TouchableOpacity onPress={() => clean()}>
+              <Text style={styles.letter}>clean</Text>
+            </TouchableOpacity>
 
         <View style={styles.guessedWordsContainer}>
-          <FlatList
-            data={guessedWords}
-            renderItem={({ item }) => <Text style={styles.word}>{item}</Text>}
-            keyExtractor={(item) => item}
-          />
+        <FlatList
+        data={guessedWords}
+        renderItem={({ item }) => <Text style={styles.word}>{item}</Text>}
+        keyExtractor={(item, index) => index.toString()}  
+      />
+
         </View>
       </View>
     </View>
@@ -131,6 +185,16 @@ const styles = StyleSheet.create({
     height: 48,
     lineHeight: 24,
   },
+  letter: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    margin: 6,
+    backgroundColor: '#ececec',
+    padding: 12,
+    borderRadius: 8,
+    textAlign: 'center',
+  
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -159,6 +223,12 @@ const styles = StyleSheet.create({
     color: '#555',
     marginVertical: 4,
     textAlign: 'center',
+  },
+  hintButton: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'blue',
+    marginTop: 10,
   },
 });
 
